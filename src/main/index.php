d7fea2c15e597973dc483bb40db7373916471852
THMKruemel <lennartsigge@icloud.com>
2024-04-30 19:48:24 +0200

    make it possible to work on the project on linux with docker. Structure project

--- src/main/index.php
+++ src/main/index.php
@@ -8,31 +8,11 @@
     <title>Formular</title>
 </head>
 <body>
-<h2>Formular</h2>
+<h1>UrlShortener</h1>
 
-<?php
-    // Hier beginnt der PHP-Code
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Verarbeite das Formular, nachdem Daten gesendet wurden
-        $name = $_POST["name"];
-        $email = $_POST["email"];
 
-        // Führe hier weitere Aktionen mit den gesendeten Daten aus, z. B. Speichern in einer Datenbank
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
 
-        // Beispiel: Ausgabe der gesendeten Daten
-        echo "Name: " . $name . "<br>";
-echo "E-Mail: " . $email;
-}
-// Hier endet der PHP-Code
-?>
-
-<!-- Das Formular sendet die Daten an dieselbe Seite (index.php) mit der POST-Methode -->
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-<label for="name">Name:</label><br>
-<input type="text" id="name" name="name"><br>
-<label for="email">E-Mail:</label><br>
-<input type="email" id="email" name="email"><br><br>
-<input type="submit" value="Absenden">
 </form>
 </body>
 </html>
